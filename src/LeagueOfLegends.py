95c170f15a5dd4c0c814cf4e854d9488d9c9a31d
Zeke <accounts@zeke.sh>
2020-10-12 17:53:50 -0500

    can now read total spends in league of legends

--- src/LeagueOfLegends.py
+++ src/LeagueOfLegends.py
@@ -1,6 +1,23 @@
+import os
+import glob
+import json
+from pathlib import Path
+
 class LeagueOfLegends():
     def total_spent_on_league(self):
-        print('test')
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        path = Path(dir_path)
+        path = path.parent
+        path = str(path) + '/data/leagueoflegends/rp_purchase_history.json'
+        with open(path, encoding='utf-8', mode='r') as f:
+            json_data = json.load(f)
+
+        amount = 0
+
+        for i in json_data:
+            amount = amount + i['amount']
+
+        print('You spent ' + str(round(amount, 2)))
 
 
 
